92cacf6dd525c8b7e0ff61c3fe854b21d18e0b6a
Daiana de Paula <daianaadepaula1@gmail.com>
2022-02-22 09:48:39 -0300

    Adding access to an API. Using async/await.

--- scripts.js
+++ scripts.js
@@ -1,38 +1,40 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById("currency-select")
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 0.0000042
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-reaL').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValuesText = document.getElementById('currency-value-text')
 
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
     ).format(inputReais)
 
-    if(select.value === "US$ Dólar americano") {
+    if (select.value === "US$ Dólar americano") {
         currencyValuesText.innerHTML = new Intl.NumberFormat('en-US',
-        { style: 'currency', currency: 'USD' }
-    ).format(inputReais / dolar)
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar)
     }
 
-    if(select.value === "€ Euro") {
+    if (select.value === "€ Euro") {
         currencyValuesText.innerHTML = new Intl.NumberFormat('de-DE',
-        { style: 'currency', currency: 'EUR' }
-    ).format(inputReais / euro)
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro)
     }
 
-    if(select.value === "Bitcoin"){
+    if (select.value === "Bitcoin") {
         currencyValuesText.innerHTML = new Intl.NumberFormat('de-DE',
-        { style: 'currency', currency: 'BTC' }
-    ).format(inputReais / bitcoin)
+            { style: 'currency', currency: 'BTC' }
+        ).format(inputReais / bitcoin)
     }
-    
+
 }
 
 
